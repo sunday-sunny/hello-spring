2a88b44781335f82f4358f1ad7de23b0317f5705
sunday-sunny <mallox22@hanyang.ac.kr>
2021-07-23 16:48:19 +0900

    Spring Data JPA 적용

--- hello-spring/src/main/java/hello/hellospring/springConfig.java
+++ hello-spring/src/main/java/hello/hellospring/springConfig.java
@@ -2,33 +2,30 @@ package hello.hellospring;
 
 import hello.hellospring.repository.JpaMemberRepository;
 import hello.hellospring.repository.MemberRepository;
-import hello.hellospring.repository.MemoryMemberRepository;
 import hello.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import javax.persistence.EntityManager;
-
 @Configuration
 public class springConfig {
 
-    private final EntityManager em;
+    private final MemberRepository memberRepository;
 
     @Autowired
-    public springConfig(EntityManager em) {
-        this.em = em;
+    public springConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
     }
 
     @Bean
     public MemberService memberService() {
-        return new MemberService(memberRepository());
+        return new MemberService(memberRepository);
     }
 
-    @Bean
-    public MemberRepository memberRepository() {
-        //return new MemoryMemberRepository();
-        return new JpaMemberRepository(em);
-    }
+//    @Bean
+//    public MemberRepository memberRepository() {
+//        //return new MemoryMemberRepository();
+//        return new JpaMemberRepository(em);
+//    }
 
 }
