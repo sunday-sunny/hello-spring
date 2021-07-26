62175aa0a98fd10d8c38fe1b2ed6cf403f2b1b06
sunday-sunny <mallox22@hanyang.ac.kr>
2021-07-26 19:40:15 +0900

    testcase 주석 변경

--- hello-spring/src/test/java/hello/hellospring/repository/MemoryMemberRepositoryTest.java
+++ hello-spring/src/test/java/hello/hellospring/repository/MemoryMemberRepositoryTest.java
@@ -20,16 +20,21 @@ class MemoryMemberRepositoryTest {
 
     @Test
     public void save(){
+        //given
         Member member = new Member();
         member.setName("spring");
 
+        //when
         repository.save(member);
+
+        //then
         Member result = repository.findById(member.getId()).get();
         assertThat(member).isEqualTo(result);
     }
 
     @Test
     public void findByName(){
+        //given
         Member member1 = new Member();
         member1.setName("spring1");
         repository.save(member1);
@@ -38,12 +43,16 @@ class MemoryMemberRepositoryTest {
         member2.setName("spring2");
         repository.save(member2);
 
+        //when
         Member result = repository.findByName("spring1").get();
+
+        //then
         assertThat(result).isEqualTo(member1);
     }
 
     @Test
     public void findAll() {
+        //given
         Member member1 = new Member();
         member1.setName("spring1");
         repository.save(member1);
@@ -52,7 +61,10 @@ class MemoryMemberRepositoryTest {
         member2.setName("spring2");
         repository.save(member2);
 
+        //when
         List<Member> result = repository.findAll();
+
+        //then
         assertThat(result.size()).isEqualTo(2);
     }
 
